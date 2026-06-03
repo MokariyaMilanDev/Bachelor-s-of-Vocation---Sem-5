01971be3461de8ecf62a46d2a8ce3acf955082ee
MokariyaMilanDev <mokariyamilan.dev+GitHub@gmail.com>
2026-06-03 07:18:14 +0530

    (Learn) | .NET | Variables

---
+++ dot-net/first-console-project/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter Your Name: ");
+string name = Console.ReadLine();
+Console.WriteLine("Hello, " + name + "!");
+
