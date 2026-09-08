9dce00acb46b89203dbef12ef8bfa83e4097263d
MokariyaMilanDev <mokariyamilan.dev+GitHub@gmail.com>
2026-09-08 08:55:49 +0530

    (Create) | C# - .NET | razor pages test project

---
+++ dot-net/programs/first-razor-pages/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webapp.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public string Greet { get; set; } 
+
+    public void OnGet()
+    {
+        Greet = "Love from Devs!";
+    }
+}
